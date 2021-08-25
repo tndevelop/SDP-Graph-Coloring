92d1d8f71eda826270c90b65a8578bf404f648f2
TOMMASO NATTA s282478 <s282478@studenti.polito.it>
2021-08-25 13:17:04 +0200

    algorithm selected through program parameter

--- main.cpp
+++ main.cpp
@@ -7,39 +7,79 @@
 
 using namespace std;
 
-//first parameter determines the graph that will be used as input
+/**
+ * @parameters
+ * 1) graph that will be used as input (number from 0 to graphPaths length )
+ * 2) algorithm that will be run (
+ *          0 = greedy
+ *          1 = JP sequential
+ *          2 = JP parallel
+ * )
+ */
+
+//first parameter determines the
 int main(int argc, char ** argv) {
     int maxColUsed = -1;
     string basePath = "./graphs/benchmark/";
-    string graphPaths [] = {"manual/v10.gra", "manual/v100.gra", "small_sparse_real/agrocyc_dag_uniq.gra", "small_sparse_real/human_dag_uniq.gra", "small_dense_real/arXiv_sub_6000-1.gra", "scaleFree/ba10k5d.gra",
+    string graphPaths [] = {"manual/v10.gra"/*1KB*/, "manual/v100.gra"/*13KB*/, "small_sparse_real/agrocyc_dag_uniq.gra"/*1MB*/, "small_sparse_real/human_dag_uniq.gra"/*0.5MB*/, "small_dense_real/arXiv_sub_6000-1.gra"/*0.3MB*/, "scaleFree/ba10k5d.gra"/*0.2MB*/,
             // the next files are too large for git, need to import the "large" folder under "benchmark". It is already ignored in the .gitignore file
-                            "large/uniprotenc_22m.scc.gra", "large/go_uniprot.gra" };
-    string finalPath = basePath + graphPaths[atoi(argv[1])];
+                            "large/uniprotenc_150m.scc.gra"/*2MB*/, "large/citeseer.scc.gra"/*8MB*/, "large/uniprotenc_22m.scc.gra"/*19MB*/, "large/go_uniprot.gra"/*255MB*/ };
+    string selectedGraph = graphPaths[atoi(argv[1])];
+    string finalPath = basePath + selectedGraph;
+
+    string algorithms [] = {"greedy", "JP sequential", "JP parallel"};
+    string selectedAlg = algorithms[atoi(argv[2])];
+
+    cout << "running " << selectedAlg << " algorithm on graph " << selectedGraph << endl << endl ;
 
     map<int, list<int>> graph = readGraph(finalPath);
 
     vector<int> colors = initializeLabels(graph.size());
 
-    vector<int> colorsGreedy = greedyAssignment(graph, colors, &maxColUsed);
+    switch(atoi(argv[2])){
 
-    //some output just to be sure the application ran properly
-    cout << "number of nodes: " << graph.size() << endl;
-    cout << "number of greedy colors: " << maxColUsed + 1 << endl;
-    cout << "for instance color " << colorsGreedy[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+        case 0:{
+            vector<int> colorsGreedy = greedyAssignment(graph, colors, &maxColUsed);
+
+            //some output just to be sure the application ran properly
+            cout << "number of nodes: " << graph.size() << endl;
+            cout << "number of greedy colors: " << maxColUsed + 1 << endl;
+            cout << "for instance color " << colorsGreedy[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+
+            break;
+        }
+        case 1:{
+            vector<int> colorsJPS = jonesPlassmannSequentialAssignment(graph, colors, &maxColUsed);
+
+            //some output just to be sure the application ran properly
+            cout << "number of nodes: " << graph.size() << endl;
+            cout << "number of JP colors: " << maxColUsed + 1 << endl;
+            cout << "for instance color " << colorsJPS[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+
+            break;
+        }
+        case 2:{
+            vector<int> colorsJP = jonesPlassmannParallelAssignment(graph, colors, &maxColUsed);
+
+            //some output just to be sure the application ran properly
+            cout << "number of nodes: " << graph.size() << endl;
+            cout << "number of JP colors: " << maxColUsed + 1 << endl;
+            cout << "for instance color " << colorsJP[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+
+            break;
+        }
+        default:{
+            cout << "Selected non-existing algorithm";
+            break;
+        }
+    }
+
+
+
 
     maxColUsed = -1;
-    vector<int> colorsJPS = jonesPlassmannSequentialAssignment(graph, colors, &maxColUsed);
-
-    //some output just to be sure the application ran properly
-    cout << "number of JP colors: " << maxColUsed + 1 << endl;
-    cout << "for instance color " << colorsJPS[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
 
     maxColUsed = -1;
-    vector<int> colorsJP = jonesPlassmannParallelAssignment(graph, colors, &maxColUsed);
-
-    //some output just to be sure the application ran properly
-    cout << "number of JP colors: " << maxColUsed + 1 << endl;
-    cout << "for instance color " << colorsJP[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
 
     return 0;
 }
