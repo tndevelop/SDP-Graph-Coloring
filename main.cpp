914aa70bdf81f7fcb22452e960b33ed2e0e94a08
TOMMASO NATTA s282478 <s282478@studenti.polito.it>
2021-08-28 12:31:15 +0200

    added menu mode (avoid I/O on every run)

--- main.cpp
+++ main.cpp
@@ -23,108 +23,137 @@ using namespace std;
  * )
  */
 
-//first parameter determines the
 int main(int argc, char ** argv) {
     int maxColUsed = -1;
     string basePath = "./graphs/benchmark/";
-    string graphPaths [] = {/*0)*/"manual/v10.gra"/*1KB*/, "manual/v100.gra"/*13KB*/, "manual/v1000.gra"/*1.6MB*/, 
+    string algorithms [] = {"greedy", "JP sequential", "JP parallel","SDL sequential","SDL parallel", "MIS sequential", "MIS parallel"};
+    string graphPaths [] = {/*0)*/"manual/v10.gra"/*1KB*/, "manual/v100.gra"/*13KB*/, "manual/v1000.gra"/*1.6MB*/,
             /*3)*/"small_sparse_real/agrocyc_dag_uniq.gra"/*1MB*/, "small_sparse_real/human_dag_uniq.gra"/*0.5MB*/, "small_dense_real/arXiv_sub_6000-1.gra"/*0.3MB*/, "scaleFree/ba10k5d.gra"/*0.2MB*/,
             // the next files are too large for git, need to import the "large" folder under "benchmark". It is already ignored in the .gitignore file
             /*7)*/"large/uniprotenc_150m.scc.gra"/*2MB*/, "large/citeseer.scc.gra"/*8MB*/, "large/uniprotenc_22m.scc.gra"/*19MB*/, "large/go_uniprot.gra"/*255MB*/ };
+    string finalPath;
+    string selectedAlg;
+    bool menuMode = false;
+
     string selectedGraph = graphPaths[atoi(argv[1])];
-    string finalPath = basePath + selectedGraph;
+    finalPath = basePath + selectedGraph;
+    if(argc >= 3){
 
-    string algorithms [] = {"greedy", "JP sequential", "JP parallel","SDL sequential","SDL parallel", "MIS sequential", "MIS parallel"};
-    string selectedAlg = algorithms[atoi(argv[2])];
+        selectedAlg = algorithms[atoi(argv[2])];
+        cout << endl << "-------------------------------------------------------------------------" << endl;
+        cout << "running " << selectedAlg << " algorithm on graph " << selectedGraph << endl << endl ;
+    }else{
+        menuMode = true;
 
-    cout << endl << "-------------------------------------------------------------------------" << endl;
-    cout << "running " << selectedAlg << " algorithm on graph " << selectedGraph << endl << endl ;
+        cout << "menu mode selected, you'll be asked to choose an algorithm shortly" << endl;
+        cout << "graph " << selectedGraph << " was selected " << endl ;
+        }
+
     map<int, list<int>> graph = readGraph(finalPath);
     vector<int> colors = initializeLabels(graph.size());
-    chrono::time_point<chrono::system_clock> startTime = chrono::system_clock::now();
 
-    
-    switch(atoi(argv[2])){
 
-        case 0:{
-            vector<int> colorsGreedy = greedyAssignment(graph, colors, &maxColUsed);
+    do {
+        int alg;
+        if(menuMode){
+            cout << endl << "select algorithm:" << endl;
 
-            //some output just to be sure the application ran properly
-            cout << "number of nodes: " << graph.size() << endl;
-            cout << "number of greedy colors: " << maxColUsed + 1 << endl;
-            cout << "for instance color " << colorsGreedy[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
-
-            break;
+            for(int i =0 ; i< (sizeof(algorithms) / sizeof(algorithms[0]))  ; i++) {
+                cout << i << ": " << algorithms[i] << endl;
+            }
+            cout << "any other number to exit" << endl;
+            cin >> alg;
+            if(alg >= sizeof(algorithms))
+                break;
+            cout << "running " << algorithms[alg] << " algorithm on graph " << selectedGraph << endl << endl ;
+        }else{
+            alg = atoi(argv[2]);
         }
-        case 1:{
-            vector<int> colorsJPS = jonesPlassmannSequentialAssignment(graph, colors, &maxColUsed);
+        chrono::time_point<chrono::system_clock> startTime = chrono::system_clock::now();
 
-            //some output just to be sure the application ran properly
-            cout << "number of nodes: " << graph.size() << endl;
-            cout << "number of JP colors: " << maxColUsed + 1 << endl;
-            cout << "for instance color " << colorsJPS[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
 
-            break;
+        switch(alg){
+
+            case 0:{
+                vector<int> colorsGreedy = greedyAssignment(graph, colors, &maxColUsed);
+
+                //some output just to be sure the application ran properly
+                cout << "number of nodes: " << graph.size() << endl;
+                cout << "number of greedy colors: " << maxColUsed + 1 << endl;
+                cout << "for instance color " << colorsGreedy[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+
+                break;
+            }
+            case 1:{
+                vector<int> colorsJPS = jonesPlassmannSequentialAssignment(graph, colors, &maxColUsed);
+
+                //some output just to be sure the application ran properly
+                cout << "number of nodes: " << graph.size() << endl;
+                cout << "number of JP colors: " << maxColUsed + 1 << endl;
+                cout << "for instance color " << colorsJPS[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+
+                break;
+            }
+            case 2:{
+                vector<int> colorsJP = jonesPlassmannParallelAssignment(graph, colors, &maxColUsed);
+
+                //some output just to be sure the application ran properly
+                cout << "number of nodes: " << graph.size() << endl;
+                cout << "number of JP colors: " << maxColUsed + 1 << endl;
+                cout << "for instance color " << colorsJP[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+
+                break;
+            }
+            case 3:{
+                vector<int> colorsSDLS = smallestDegreeLastSequentialAssignment(graph, colors, &maxColUsed);
+
+
+                //some output just to be sure the application ran properly
+                cout << "number of nodes: " << graph.size() << endl;
+                cout << "number of SDLS colors: " << maxColUsed + 1 << endl;
+                cout << "for instance color " << colorsSDLS[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+                break;
+
+            }
+            case 4:{
+                vector<int> colorsSDLP = smallestDegreeLastParallelAssignment(graph, colors, &maxColUsed);
+
+                //some output just to be sure the application ran properly
+                cout << "number of nodes: " << graph.size() << endl;
+                cout << "number of SDLP colors: " << maxColUsed + 1 << endl;
+                cout << "for instance color " << colorsSDLP[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+                break;
+            }
+
+            case 5: {
+                vector<int> colorsMIS = misSequentialAssignment(graph, colors, &maxColUsed);
+
+                //some output just to be sure the application ran properly
+                cout << "number of nodes: " << graph.size() << endl;
+                cout << "number of MIS colors: " << maxColUsed + 1 << endl;
+                cout << "for instance color " << colorsMIS[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+                break;
+            }
+
+            case 6: {
+                vector<int> colorsMISP = misParallelAssignment(graph, colors, &maxColUsed);
+
+                //some output just to be sure the application ran properly
+                cout << "number of nodes: " << graph.size() << endl;
+                cout << "number of MIS colors: " << maxColUsed + 1 << endl;
+                cout << "for instance color " << colorsMISP[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
+                break;
+            }
+
+            default:{
+                cout << "Selected non-existing algorithm";
+                break;
+            }
         }
-        case 2:{
-            vector<int> colorsJP = jonesPlassmannParallelAssignment(graph, colors, &maxColUsed);
 
-            //some output just to be sure the application ran properly
-            cout << "number of nodes: " << graph.size() << endl;
-            cout << "number of JP colors: " << maxColUsed + 1 << endl;
-            cout << "for instance color " << colorsJP[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
-
-            break;
-        }
-        case 3:{
-            vector<int> colorsSDLS = smallestDegreeLastSequentialAssignment(graph, colors, &maxColUsed);
-
-
-            //some output just to be sure the application ran properly
-            cout << "number of nodes: " << graph.size() << endl;
-            cout << "number of SDLS colors: " << maxColUsed + 1 << endl;
-            cout << "for instance color " << colorsSDLS[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
-
-        }
-        case 4:{
-            vector<int> colorsSDLP = smallestDegreeLastParallelAssignment(graph, colors, &maxColUsed);
-
-            //some output just to be sure the application ran properly
-            cout << "number of nodes: " << graph.size() << endl;
-            cout << "number of SDLP colors: " << maxColUsed + 1 << endl;
-            cout << "for instance color " << colorsSDLP[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
-        }
-
-        case 5: {
-            vector<int> colorsMIS = misSequentialAssignment(graph, colors, &maxColUsed);
-
-            //some output just to be sure the application ran properly
-            cout << "number of nodes: " << graph.size() << endl;
-            cout << "number of MIS colors: " << maxColUsed + 1 << endl;
-            cout << "for instance color " << colorsMIS[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
-        }
-
-        case 6: {
-            vector<int> colorsMISP = misParallelAssignment(graph, colors, &maxColUsed);
-
-            //some output just to be sure the application ran properly
-            cout << "number of nodes: " << graph.size() << endl;
-            cout << "number of MIS colors: " << maxColUsed + 1 << endl;
-            cout << "for instance color " << colorsMISP[maxColUsed] << " was assigned to node " << maxColUsed << endl; //should never be -1
-        }
-
-        default:{
-            cout << "Selected non-existing algorithm";
-            break;
-        }
-    }
-
-    chrono::time_point<chrono::system_clock> endTime = chrono::system_clock::now();
-    cout << "Time taken: " << chrono::duration_cast<chrono::milliseconds>(endTime - startTime).count() << " milliseconds" << endl;
-
-    maxColUsed = -1;
-
-    maxColUsed = -1;
+        chrono::time_point<chrono::system_clock> endTime = chrono::system_clock::now();
+        cout << "Time taken: " << chrono::duration_cast<chrono::milliseconds>(endTime - startTime).count() << " milliseconds" << endl;
+    }while(menuMode == true);
 
     return 0;
 }
