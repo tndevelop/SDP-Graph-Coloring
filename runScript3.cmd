bb66232211d9a9031d20c10dff1e752aaf6ced54
TOMMASO NATTA s282478 <s282478@studenti.polito.it>
2021-09-02 07:59:11 +0200

    runScript modification, tables fill and graphs 8-9 benchmarks

--- runScript3.cmd
+++ runScript3.cmd
@@ -18,35 +18,3 @@ GraphColouringLargeMem.exe 9 2 4 > cmd_out/graph_9_JP_parallel_threads_4.txt
 GraphColouringLargeMem.exe 9 2 8 > cmd_out/graph_9_JP_parallel_threads_8.txt
 GraphColouringLargeMem.exe 9 5 > cmd_out/graph_9_MIS_sequential.txt
 GraphColouringLargeMem.exe 9 6 2 > cmd_out/graph_9_MIS_parallel_threads_2.txt
-GraphColouringLargeMem.exe 9 6 4 > cmd_out/graph_9_MIS_parallel_threads_4.txt
-GraphColouringLargeMem.exe 9 6 8 > cmd_out/graph_9_MIS_parallel_threads_8.txt
-GraphColouringLargeMem.exe 9 7 1 > cmd_out/graph_9_LDF_sequential.txt
-GraphColouringLargeMem.exe 9 7 2 > cmd_out/graph_9_LDF_parallel_threads_2.txt
-GraphColouringLargeMem.exe 9 7 4 > cmd_out/graph_9_LDF_parallel_threads_4.txt
-GraphColouringLargeMem.exe 9 7 8 > cmd_out/graph_9_LDF_parallel_threads_8.txt
-GraphColouringLargeMem.exe 10 0 > cmd_out/graph_10_greedy.txt
-GraphColouringLargeMem.exe 10 1 > cmd_out/graph_10_JP_sequential.txt
-GraphColouringLargeMem.exe 10 2 2 > cmd_out/graph_10_JP_parallel_threads_2.txt
-GraphColouringLargeMem.exe 10 2 4 > cmd_out/graph_10_JP_parallel_threads_4.txt
-GraphColouringLargeMem.exe 10 2 8 > cmd_out/graph_10_JP_parallel_threads_8.txt
-GraphColouringLargeMem.exe 10 7 1 > cmd_out/graph_10_LDF_sequential.txt
-GraphColouringLargeMem.exe 10 7 2 > cmd_out/graph_10_LDF_parallel_threads_2.txt
-GraphColouringLargeMem.exe 10 7 4 > cmd_out/graph_10_LDF_parallel_threads_4.txt
-GraphColouringLargeMem.exe 10 7 8 > cmd_out/graph_10_LDF_parallel_threads_8.txt
-GraphColouringLargeMem.exe 13 0 > cmd_out/graph_13_greedy.txt
-GraphColouringLargeMem.exe 13 1 > cmd_out/graph_13_JP_sequential.txt
-GraphColouringLargeMem.exe 13 2 2 > cmd_out/graph_13_JP_parallel_threads_2.txt
-GraphColouringLargeMem.exe 13 2 4 > cmd_out/graph_13_JP_parallel_threads_4.txt
-GraphColouringLargeMem.exe 13 2 8 > cmd_out/graph_13_JP_parallel_threads_8.txt
-GraphColouringLargeMem.exe 13 7 1 > cmd_out/graph_13_LDF_sequential.txt
-GraphColouringLargeMem.exe 13 7 2 > cmd_out/graph_13_LDF_parallel_threads_2.txt
-GraphColouringLargeMem.exe 13 7 4 > cmd_out/graph_13_LDF_parallel_threads_4.txt
-GraphColouringLargeMem.exe 13 7 8 > cmd_out/graph_13_LDF_parallel_threads_8.txt
-GraphColouringLargeMem.exe 10 5 > cmd_out/graph_10_MIS_sequential.txt
-GraphColouringLargeMem.exe 10 6 2 > cmd_out/graph_10_MIS_parallel_threads_2.txt
-GraphColouringLargeMem.exe 10 6 4 > cmd_out/graph_10_MIS_parallel_threads_4.txt
-GraphColouringLargeMem.exe 10 6 8 > cmd_out/graph_10_MIS_parallel_threads_8.txt
-GraphColouringLargeMem.exe 13 5 > cmd_out/graph_13_MIS_sequential.txt
-GraphColouringLargeMem.exe 13 6 2 > cmd_out/graph_13_MIS_parallel_threads_2.txt
-GraphColouringLargeMem.exe 13 6 4 > cmd_out/graph_13_MIS_parallel_threads_4.txt
-GraphColouringLargeMem.exe 13 6 8 > cmd_out/graph_13_MIS_parallel_threads_8.txt
